074cf0f8d777fab509d70bca9c61b30e9179fa3c
William Ray Johnson <willrayjohnson@gmail.com>
2018-04-26 15:10:33 -0500

    Move create table statements around so tables were created in the proper order

--- MySQL/CREATE_SOCCER_DB.sql
+++ MySQL/CREATE_SOCCER_DB.sql
@@ -2,6 +2,19 @@ CREATE DATABASE soccer_db;
 
 USE soccer_db;
 
+CREATE TABLE `coach` (
+  `Coach_ID` varchar(45) NOT NULL,
+  `Name` varchar(45) NOT NULL,
+  PRIMARY KEY (`Coach_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `referee` (
+  `Ref_ID` varchar(45) NOT NULL,
+  `Name` varchar(45) NOT NULL,
+  `League` varchar(45) NOT NULL,
+  PRIMARY KEY (`Ref_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `team` (
   `Name` varchar(45) NOT NULL,
   `Wins` int(11) NOT NULL,
@@ -14,6 +27,31 @@ CREATE TABLE `team` (
   PRIMARY KEY (`Team_ID`),
   KEY `Coach_ID_idx` (`Coach_ID`),
   CONSTRAINT `Coach_ID` FOREIGN KEY (`Coach_ID`) REFERENCES `coach` (`Coach_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `stadium` (
+  `Stadium_ID` varchar(45) NOT NULL,
+  `Location` varchar(45) NOT NULL,
+  `Name` varchar(45) NOT NULL,
+  `Team_ID` varchar(45) NOT NULL DEFAULT 'Unaffiliated',
+  PRIMARY KEY (`Stadium_ID`),
+  KEY `Team_ID_idx` (`Team_ID`),
+  CONSTRAINT `Team_ID____` FOREIGN KEY (`Team_ID`) REFERENCES `team` (`Team_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `game` (
+  `Game_ID` varchar(45) NOT NULL,
+  `Host_team` varchar(45) NOT NULL,
+  `Guest_team` varchar(45) NOT NULL,
+  `Date` datetime NOT NULL,
+  `H_score` varchar(45) NOT NULL,
+  `G_score` varchar(45) NOT NULL,
+  `Stadium_ID` varchar(45) NOT NULL,
+  `Winner` varchar(45) NOT NULL,
+  `Loser` varchar(45) NOT NULL,
+  PRIMARY KEY (`Game_ID`),
+  KEY `Stadium_ID_idx` (`Stadium_ID`),
+  CONSTRAINT `Stadium_ID` FOREIGN KEY (`Stadium_ID`) REFERENCES `stadium` (`Stadium_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `team_penalty` (
@@ -31,38 +69,6 @@ CREATE TABLE `team_penalty` (
   CONSTRAINT `Team_ID_` FOREIGN KEY (`Team_ID`) REFERENCES `team` (`Team_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-CREATE TABLE `stadium` (
-  `Stadium_ID` varchar(45) NOT NULL,
-  `Location` varchar(45) NOT NULL,
-  `Name` varchar(45) NOT NULL,
-  `Team_ID` varchar(45) NOT NULL DEFAULT 'Unaffiliated',
-  PRIMARY KEY (`Stadium_ID`),
-  KEY `Team_ID_idx` (`Team_ID`),
-  CONSTRAINT `Team_ID____` FOREIGN KEY (`Team_ID`) REFERENCES `team` (`Team_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE `referee` (
-  `Ref_ID` varchar(45) NOT NULL,
-  `Name` varchar(45) NOT NULL,
-  `League` varchar(45) NOT NULL,
-  PRIMARY KEY (`Ref_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE `players_of_game` (
-  `FIFA_ID` varchar(45) NOT NULL,
-  `Game_ID` varchar(45) NOT NULL,
-  `is_Sub` varchar(45) DEFAULT NULL,
-  `Position` varchar(45) NOT NULL,
-  PRIMARY KEY (`FIFA_ID`,`Game_ID`),
-  KEY `Game_ID_idx` (`Game_ID`),
-  CONSTRAINT `FIFA_ID` FOREIGN KEY (`FIFA_ID`) REFERENCES `player` (`FIFA_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `Game_ID` FOREIGN KEY (`Game_ID`) REFERENCES `game` (`Game_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
 CREATE TABLE `player` (
   `FIFA_ID` varchar(45) NOT NULL,
   `Num` int(2) NOT NULL,
@@ -74,6 +80,16 @@ CREATE TABLE `player` (
   CONSTRAINT `Team_ID___` FOREIGN KEY (`Team_ID`) REFERENCES `team` (`Team_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `players_of_game` (
+  `FIFA_ID` varchar(45) NOT NULL,
+  `Game_ID` varchar(45) NOT NULL,
+  `is_Sub` varchar(45) DEFAULT NULL,
+  `Position` varchar(45) NOT NULL,
+  PRIMARY KEY (`FIFA_ID`,`Game_ID`),
+  KEY `Game_ID_idx` (`Game_ID`),
+  CONSTRAINT `FIFA_ID` FOREIGN KEY (`FIFA_ID`) REFERENCES `player` (`FIFA_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `Game_ID` FOREIGN KEY (`Game_ID`) REFERENCES `game` (`Game_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `league` (
   `Name` varchar(45) NOT NULL,
@@ -89,33 +105,6 @@ CREATE TABLE `injury` (
   CONSTRAINT `FIFA_ID_` FOREIGN KEY (`FIFA_ID`) REFERENCES `player` (`FIFA_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-CREATE TABLE `game` (
-  `Game_ID` varchar(45) NOT NULL,
-  `Host_team` varchar(45) NOT NULL,
-  `Guest_team` varchar(45) NOT NULL,
-  `Date` datetime NOT NULL,
-  `H_score` varchar(45) NOT NULL,
-  `G_score` varchar(45) NOT NULL,
-  `Stadium_ID` varchar(45) NOT NULL,
-  `Winner` varchar(45) NOT NULL,
-  `Loser` varchar(45) NOT NULL,
-  PRIMARY KEY (`Game_ID`),
-  KEY `Stadium_ID_idx` (`Stadium_ID`),
-  CONSTRAINT `Stadium_ID` FOREIGN KEY (`Stadium_ID`) REFERENCES `stadium` (`Stadium_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE `coach` (
-  `Coach_ID` varchar(45) NOT NULL,
-  `Name` varchar(45) NOT NULL,
-  `Team_ID` varchar(45) NOT NULL,
-  PRIMARY KEY (`Coach_ID`),
-  KEY `Team_ID_idx` (`Team_ID`),
-  CONSTRAINT `Team_ID` FOREIGN KEY (`Team_ID`) REFERENCES `team` (`Team_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
 CREATE TABLE `card_penalty` (
   `Color` varchar(45) NOT NULL,
   `FIFA_ID` varchar(45) NOT NULL,
@@ -130,7 +119,6 @@ CREATE TABLE `card_penalty` (
   CONSTRAINT `Ref_ID__` FOREIGN KEY (`Ref_ID`) REFERENCES `referee` (`Ref_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
 CREATE TABLE `refs_of_games` (
   `Ref_ID` varchar(45) NOT NULL,
   `Game_ID` varchar(45) NOT NULL,
@@ -140,7 +128,6 @@ CREATE TABLE `refs_of_games` (
   CONSTRAINT `Game_ID____` FOREIGN KEY (`Game_ID`) REFERENCES `game` (`Game_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `Ref_ID____` FOREIGN KEY (`Ref_ID`) REFERENCES `referee` (`Ref_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 CREATE TABLE `coach_of_game` (
   `Coach_ID` varchar(45) NOT NULL,
