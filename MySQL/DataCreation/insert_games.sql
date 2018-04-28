beb0ce2492d2ce70411dcf029c1c7f50b9a2ecc3
William Johnson <willrayjohnson@gmail.com>
2018-04-28 12:50:16 -0500

    Changed to CHAR

--- MySQL/DataCreation/insert_games.sql
+++ MySQL/DataCreation/insert_games.sql
@@ -1,10 +1,8 @@
-CREATE PROCEDURE `insert_games` ()
+CREATE PROCEDURE `insert_games` (IN gameCount INT)
 BEGIN
-    DECLARE gameCount INT DEFAULT 0;
     DECLARE hScore INT DEFAULT 0;
     DECLARE gScore INT DEFAULT 0;
     DECLARE gamei INT DEFAULT 0;
-    SET gameCount = 2000000;
 
     create_game: LOOP
         IF gamei >= gameCount THEN
@@ -13,11 +11,11 @@ BEGIN
         SET hScore = FLOOR(RAND()*10);
         SET gScore = FLOOR(RAND()*10);
         IF hScore > gScore THEN
-            CALL insert_host_win(CAST(gamei AS VARCHAR(45)), hScore, gScore);
+            CALL insert_host_win(CAST(gamei AS CHAR(45)), hScore, gScore);
         ELSEIF hScore < gScore THEN
-            CALL insert_guest_win(CAST(gamei AS VARCHAR(45)), hScore, gScore);
+            CALL insert_guest_win(CAST(gamei AS CHAR(45)), hScore, gScore);
         ELSE
-            CALL insert_host_win(CAST(gamei AS VARCHAR(45)), hScore+1, gScore);
+            CALL insert_host_win(CAST(gamei AS CHAR(45)), hScore+1, gScore);
         END IF;
         SET gamei = gamei + 1;
     END LOOP create_game;
